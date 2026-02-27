10a66e607475874548912c154b4d79dee1a37b2b
rosabelle <rosabellevw@gmail.com>
2026-02-27 13:26:52 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.LoopingInBackground)
+let x = [
+0,
+1,
+2,
+3,
+0,
+1,
+2,
+3,
+4
+]
+let y = [
+0,
+1,
+1,
+1,
+2,
+2,
+2,
+2,
+2
+]
+basic.forever(function () {
+    for (let i = 0; i <= x.length - 1; i++) {
+        led.plot(x[i], y[i])
+        basic.pause(100)
+        basic.clearScreen()
+    }
+})
